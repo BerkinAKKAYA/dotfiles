88877b5899689e3b41095dfa8ba3f115a0eb431d
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-01-30 08:46:02 +0300

    Simplify Swaybar

--- config/sway/swaybar.sh
+++ config/sway/swaybar.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 TEMP=$((`cat /sys/class/thermal/thermal_zone0/temp` / 1000))°
-#CPU=$(cat <(grep 'cpu ' /proc/stat) <(sleep 1 && grep 'cpu ' /proc/stat) | awk -v RS="" '{printf "%d%\n", ($13-$2+$15-$4)*100/($13-$2+$15-$4+$16-$5)}')
-#RAM=$(free -h | grep Mem | awk '{ print $3 }')
 BAT=$(cat /sys/class/power_supply/BAT0/capacity)%
 TIME=$(date "+%d %B, %A | %H:%M")
 
