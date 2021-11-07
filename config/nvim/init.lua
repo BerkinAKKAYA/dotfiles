51b5620cb2e21233c836bcc6dd12e92ea27b594e
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-11-07 11:22:49 +0000

    update syntax

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
 local vim = vim
-local api = vim.api
 local cmp = require('cmp')
+local map = vim.api.nvim_set_keymap
 
 vim.g.gitblame_date_format = '%r'
 vim.g.material_style = "deep ocean"
@@ -34,6 +34,7 @@ vim.wo.foldminlines = 1
 vim.opt.foldtext = 'v:lua.custom_fold_text()'
 
 vim.cmd 'colorscheme material'
+vim.cmd 'set tabstop=4'
 
 require 'paq' {
 	'savq/paq-nvim';
@@ -81,31 +82,15 @@ require 'paq' {
 	'lewis6991/gitsigns.nvim';
 }
 
-api.nvim_command('au BufWinEnter * set tabstop=4')
+vim.api.nvim_command("command WW :execute ':silent w !doas tee % > /dev/null' | :edit!")
+vim.api.nvim_command("command W :execute ':w'")
+vim.api.nvim_command("command Q :execute ':q'")
 
-api.nvim_command("command WW :execute ':silent w !doas tee % > /dev/null' | :edit!")
-api.nvim_command("command W :execute ':w'")
-api.nvim_command("command Q :execute ':q'")
-
-api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank()')
--- api.nvim_command('au CursorHold * silent! lua vim.lsp.buf.hover()')
--- api.nvim_command('au BufWinLeave * mkview')
--- api.nvim_command('au BufWinEnter * silent! loadview')
-api.nvim_command('au BufWritePre *.* lua vim.lsp.buf.formatting_sync(nil, 200)')
-
--- highlights
-api.nvim_command('au VimEnter * highlight HopNextKey  guibg=#ff0000 guifg=#ffffff')
-api.nvim_command('au VimEnter * highlight HopNextKey1 guibg=#ff0000 guifg=#ffffff')
-api.nvim_command('au VimEnter * highlight HopNextKey2 guibg=#ff0000 guifg=#ffffff')
-api.nvim_command('au VimEnter * highlight HopNextKey2 guibg=#ff0000 guifg=#ffffff')
-api.nvim_command('au VimEnter * highlight TabLine guifg=#555555 guibg=#111111')
-api.nvim_command('au VimEnter * highlight TabLineSel guifg=#999999 guibg=#333333')
-api.nvim_command('au VimEnter * highlight TabLineFill guibg=#555555 gui=NONE')
-
-local removeBackgroundOf = { 'Normal', 'EndOfbuffer', 'SignColumn', 'Folded', 'LineNr' }
-for _, item in ipairs(removeBackgroundOf) do
-	api.nvim_command('au VimEnter * highlight ' .. item .. ' gui=NONE guibg=NONE')
-end
+vim.api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank()')
+vim.api.nvim_command('au CursorHold * silent! lua vim.lsp.buf.hover()')
+vim.api.nvim_command('au BufWinLeave * mkview')
+vim.api.nvim_command('au BufWinEnter * loadview')
+vim.api.nvim_command('au BufWritePre *.* lua vim.lsp.buf.formatting_sync(nil, 200)')
 
 -- Setup LSP
 local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'cssls', 'vuels' }
@@ -204,47 +189,47 @@ cmp.setup({
 	}
 })
 
-api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'F', ':HopWord<CR>', {})
-api.nvim_set_keymap('n', '<C-i>', ':NvimTreeToggle<CR>', {})
-api.nvim_set_keymap('n', '<C-f>', ':Telescope git_files<CR>', {})
-api.nvim_set_keymap('n', '<C-p>', ':Telescope neoclip a extra=star,plus,b<CR>', {})
+map('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+map('n', 'F', ':HopWord<CR>', {})
+map('n', '<C-i>', ':NvimTreeToggle<CR>', {})
+map('n', '<C-f>', ':Telescope git_files<CR>', {})
+map('n', '<C-p>', ':Telescope neoclip a extra=star,plus,b<CR>', {})
 
 -- find and replace
-api.nvim_set_keymap('n', '<Space>r', ':s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<Space>R', ':%s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'cn', '*``cgn', {})
-api.nvim_set_keymap('n', 'cN', '*``cgN', {})
+map('n', '<Space>r', ':s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
+map('n', '<Space>R', ':%s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
+map('n', 'cn', '*``cgn', {})
+map('n', 'cN', '*``cgN', {})
 
 -- lsp
-api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'gtd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+map('n', 'gD', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+map('n', 'gtd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+map('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
+map('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
 
 -- keep cursor at the center of the screen while searching
-api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
-api.nvim_set_keymap('n', 'J', 'mzJ`z', { noremap = true, silent = true })
+map('n', 'n', 'nzzzv', { noremap = true, silent = true })
+map('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+map('n', 'J', 'mzJ`z', { noremap = true, silent = true })
 
 -- undo breakpoints
-api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '.', '.<c-g>u', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true, silent = true })
-api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
+map('i', ',', ',<c-g>u', { noremap = true, silent = true })
+map('i', '.', '.<c-g>u', { noremap = true, silent = true })
+map('i', '!', '!<c-g>u', { noremap = true, silent = true })
+map('i', '?', '?<c-g>u', { noremap = true, silent = true })
 
 -- jumplist mutations
-api.nvim_set_keymap('n', 'k', "v:count > 5 ? \"m'\" . v:count . 'k' : 'k'", { noremap = true, expr = true, silent = true })
-api.nvim_set_keymap('n', 'j', "v:count > 5 ? \"m'\" . v:count . 'j' : 'j'", { noremap = true, expr = true, silent = true })
+map('n', 'k', "v:count > 5 ? \"m'\" . v:count . 'k' : 'k'", { noremap = true, expr = true, silent = true })
+map('n', 'j', "v:count > 5 ? \"m'\" . v:count . 'j' : 'j'", { noremap = true, expr = true, silent = true })
 
 -- move lines up and down
-api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<C-j>', "<esc>:m .+1<CR>i", { noremap = true, silent = true })
-api.nvim_set_keymap('i', '<C-k>', "<esc>:m .-2<CR>i", { noremap = true, silent = true })
+map('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+map('i', '<C-j>', "<esc>:m .+1<CR>i", { noremap = true, silent = true })
+map('i', '<C-k>', "<esc>:m .-2<CR>i", { noremap = true, silent = true })
 
 -- Prevent focusing LSP floating window
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { focusable = false })
