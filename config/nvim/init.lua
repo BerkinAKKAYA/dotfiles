5fad5ba7712e56c68ea380435646683bc16e5d03
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-12-19 19:48:30 +0000

    divide configuration files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,49 +1,12 @@
-local map = vim.api.nvim_set_keymap
 local coq = require("coq")
 
-vim.g.gitblame_date_format = '%r'
-vim.g.material_style       = "deep ocean"
-vim.g.closetag_filenames   = '*.html,*.blade.php,*.vue'
-vim.g.netrw_liststyle      = 3
-vim.g.netrw_banner         = 0
-vim.g.netrw_browse_split   = 0
-vim.g.netrw_winsize        = 25
-vim.g.netrw_altv           = 1
-
-vim.o.completeopt          = 'menuone,noinsert,noselect'
-vim.o.wildmode             = 'list,longest,full'
-vim.o.wildmenu             = true
-vim.o.clipboard            = 'unnamedplus'
-vim.o.termguicolors        = true
-vim.o.laststatus           = 0
-vim.o.mouse                = 'a'
-vim.o.ruler                = false
-vim.o.showmode             = false
-vim.o.ignorecase           = true
-vim.o.updatetime           = 250
-vim.o.hidden               = true
-vim.o.showcmd              = false
-vim.o.scrolloff            = 3
-vim.o.lazyredraw           = true
-vim.bo.tabstop             = 4
-vim.bo.softtabstop         = 4
-vim.bo.shiftwidth          = 4
-vim.bo.expandtab           = false
-vim.bo.syntax              = 'on'
-vim.wo.signcolumn          = 'yes:1'
-vim.wo.cursorline          = false
-vim.wo.number              = true
-vim.wo.relativenumber      = true
-vim.wo.wrap                = false
-vim.wo.foldexpr            = 'nvim_treesitter#foldexpr()'
-vim.wo.foldmethod          = 'expr'
-vim.wo.foldnestmax         = 10
-vim.wo.foldminlines        = 1
-vim.opt.foldtext           = 'v:lua.custom_fold_text()'
-vim.opt.fillchars          = { fold = " ", eob = " ", vert = "|", msgsep = " " }
-
+-- force settings (loadview etc. won't override these)
 vim.cmd 'colorscheme material'
 vim.cmd 'set tabstop=4'
+
+-- import other config files
+-- vim.cmd 'luafile ~/.config/nvim/lua/settings.lua'
+vim.cmd 'luafile ~/.config/nvim/lua/mappings.lua'
 
 require 'paq' {
 	'savq/paq-nvim';
@@ -85,58 +48,6 @@ require 'paq' {
 	'f-person/git-blame.nvim';
 	'lewis6991/gitsigns.nvim';
 }
-
--- plugins
-map('n', 'F', ':HopWord<CR>', {})
-map('n', '<C-f>', ':Telescope find_files<CR>', {})
-
--- clear highlights
-map('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
-
--- find and replace
-map('n', '<Space>r', ':s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
-map('n', '<Space>R', ':%s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
-map('n', 'cn', '*``cgn', {})
-map('n', 'cN', '*``cgN', {})
-
--- lsp
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
-map('n', 'gtd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-map('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
-map('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
-
--- keep cursor at the center of the screen while searching and joining
-map('n', 'n', 'nzzzv', { noremap = true, silent = true })
-map('n', 'N', 'Nzzzv', { noremap = true, silent = true })
-map('n', 'J', 'mzJ`z', { noremap = true, silent = true })
-
--- undo breakpoints
-map('i', ',', ',<c-g>u', { noremap = true, silent = true })
-map('i', '.', '.<c-g>u', { noremap = true, silent = true })
-map('i', '!', '!<c-g>u', { noremap = true, silent = true })
-map('i', '?', '?<c-g>u', { noremap = true, silent = true })
-
--- jumplist mutations
-map('n', 'k', "v:count > 5 ? \"m'\" . v:count . 'k' : 'k'", { noremap = true, expr = true, silent = true })
-map('n', 'j', "v:count > 5 ? \"m'\" . v:count . 'j' : 'j'", { noremap = true, expr = true, silent = true })
-
--- move lines up and down
-map('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-map('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-map('i', '<C-j>', "<esc>:m .+1<CR>i", { noremap = true, silent = true })
-map('i', '<C-k>', "<esc>:m .-2<CR>i", { noremap = true, silent = true })
-
---ctrl+hjkl to navigate splits
-map("n", "<c-k>", "<cmd>wincmd k<CR>", { noremap = true })
-map("n", "<c-j>", "<cmd>wincmd j<CR>", { noremap = true })
-map("n", "<c-h>", "<cmd>wincmd h<CR>", { noremap = true })
-map("n", "<c-l>", "<cmd>wincmd l<CR>", { noremap = true })
-
-map("n", "<C-q>", "<cmd>Lex<CR>", {})
-map("n", "<C-t>", "<cmd>tabnew<CR>", {})
 
 vim.api.nvim_command("command WW :execute ':silent w !doas tee % > /dev/null' | :edit!")
 vim.api.nvim_command("command W :execute ':w'")
---
+++ config/nvim/lua/mappings.lua
@@ -0,0 +1,53 @@
+local map = vim.api.nvim_set_keymap
+
+-- plugins
+map('n', 'F', ':HopWord<CR>', {})
+map('n', '<C-f>', ':Telescope find_files<CR>', {})
+
+-- clear highlights
+map('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+
+-- find and replace
+map('n', '<Space>r', ':s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
+map('n', '<Space>R', ':%s/<C-r><C-w>//g<Left><Left>', { noremap = true, silent = true })
+map('n', 'cn', '*``cgn', {})
+map('n', 'cN', '*``cgN', {})
+
+-- lsp
+map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+map('n', 'gtd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { noremap = true, silent = true })
+map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+map('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
+map('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
+
+-- keep cursor at the center of the screen while searching and joining
+map('n', 'n', 'nzzzv', { noremap = true, silent = true })
+map('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+map('n', 'J', 'mzJ`z', { noremap = true, silent = true })
+
+-- undo breakpoints
+map('i', ',', ',<c-g>u', { noremap = true, silent = true })
+map('i', '.', '.<c-g>u', { noremap = true, silent = true })
+map('i', '!', '!<c-g>u', { noremap = true, silent = true })
+map('i', '?', '?<c-g>u', { noremap = true, silent = true })
+
+-- jumplist mutations
+map('n', 'k', "v:count > 5 ? \"m'\" . v:count . 'k' : 'k'", { noremap = true, expr = true, silent = true })
+map('n', 'j', "v:count > 5 ? \"m'\" . v:count . 'j' : 'j'", { noremap = true, expr = true, silent = true })
+
+-- move lines up and down
+map('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+map('i', '<C-j>', "<esc>:m .+1<CR>i", { noremap = true, silent = true })
+map('i', '<C-k>', "<esc>:m .-2<CR>i", { noremap = true, silent = true })
+
+--ctrl+hjkl to navigate splits
+map("n", "<c-k>", "<cmd>wincmd k<CR>", { noremap = true })
+map("n", "<c-j>", "<cmd>wincmd j<CR>", { noremap = true })
+map("n", "<c-h>", "<cmd>wincmd h<CR>", { noremap = true })
+map("n", "<c-l>", "<cmd>wincmd l<CR>", { noremap = true })
+
+map("n", "<C-q>", "<cmd>Lex<CR>", {})
+map("n", "<C-t>", "<cmd>tabnew<CR>", {})
